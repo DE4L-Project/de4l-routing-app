d12d8d4593511b4a45a25b853b55d173cad53a2b
gaunitz <gaunitz@wifa.uni-leipzig.de>
2022-07-19 12:48:07 +0200

    - upgraded alpine version

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -28,8 +28,5 @@ bash /tmp/configure-orsm-routing.sh $OSM_FILENAME 5000
 cd /data/shortest-profile
 bash /tmp/configure-orsm-routing.sh $OSM_FILENAME 5001
 
-rm -rf /data/pollution-profile/leipzig_bikenet.json
-rm -rf /data/pollution-profile/all_data_pollution.json
-
 #68 years
 sleep 2147483647
